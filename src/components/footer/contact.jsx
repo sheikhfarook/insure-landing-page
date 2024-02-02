2b794127a3b4fa38982ee85f70f502dfd880ae83
Dada Khalandar <114235328+sheikhfarook@users.noreply.github.com>
2024-02-02 16:49:10 +0530

    tailwind css

---
+++ src/components/footer/contact.jsx
@@ -0,0 +1,57 @@
+const Contact = () => {
+  return (
+    <>
+      <div className="flex mx-40 my-10 font-kerla gap-20">
+        {/* 1 */}
+        <div className="w-[15.9375rem] space-y-10">
+          <div className="text-[#837D88] text-[0.8125rem] font-[700] tracking-[0.09375rem] ">
+            OUR COMPANY
+          </div>
+          <div className="text-[#2D2641] text-[0.8125rem] font-[700] space-y-3 tracking-[0.09375rem] ">
+            <div>HOW WE WORK</div>
+            <div>WHY INSURE?</div>
+            <div>VIEW PLANS</div>
+            <div>REVIEWS</div>
+          </div>
+          {/* */}
+        </div>
+        {/* 2 */}
+        <div className="w-[15.9375rem]  space-y-10">
+          <div className="text-[#837D88] text-[0.8125rem] font-[700] tracking-[0.09375rem] ">
+            HELP ME
+          </div>
+          <div className="text-[#2D2641] text-[0.8125rem] font-[700] space-y-3 tracking-[0.09375rem] ">
+            <div>FAQ</div>
+            <div>TERMS OF USE</div>
+            <div>PRIVACY POLICY</div>
+            <div>COOKIES</div>
+          </div>
+        </div>
+        {/* 3 */}
+        <div className="w-[15.9375rem]  space-y-10">
+          <div className="text-[#837D88] text-[0.8125rem] font-[700] tracking-[0.09375rem] ">
+            CONTACT
+          </div>
+          <div className="text-[#2D2641] text-[0.8125rem] font-[700] space-y-3 tracking-[0.09375rem] ">
+            <div>SALES</div>
+            <div>SUPPORT</div>
+            <div>LIVE CHAT</div>
+          </div>
+        </div>
+        {/* 4 */}
+        <div className="w-[15.9375rem]  space-y-10">
+          <div className="text-[#837D88] text-[0.8125rem] font-[700] tracking-[0.09375rem] ">
+            OTHERS
+          </div>
+          <div className="text-[#2D2641] text-[0.8125rem] font-[700] space-y-3 tracking-[0.09375rem] ">
+            <div>CAREERS</div>
+            <div>PRESS</div>
+            <div>PRESS</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
